987b0ca3df87e546d7363b33956231e7967a4de7
omisoshiru4738 <mai300500@gmail.com>
2020-10-31 11:29:47 +0000

    Kadai

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,18 +7,6 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //
-}
-?>
-<?php
-
-namespace App\Http\Controllers\Admin;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
-class NewsController extends Controller
-{
 	 // 以下を追記
   public function add()
   {
